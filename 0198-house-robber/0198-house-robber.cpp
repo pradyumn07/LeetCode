4b299da6fd5283866df2bc122769989beca70ce0
pradyumn07 <123253911+pradyumn07@users.noreply.github.com>
2024-08-03 21:40:50 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (26.43%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    int findWays(int n,vector<int> &nums,vector<int> &dp){
+        if(n==0) return  nums[0];
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        
+        int p=findWays(n-2,nums,dp)+nums[n];
+        int np=findWays(n-1,nums,dp)+0;
+        return dp[n]=max(p,np);
+    }
+    int rob(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return findWays(n-1,nums,dp);
+    }
+};
