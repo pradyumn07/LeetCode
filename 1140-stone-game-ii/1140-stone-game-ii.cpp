e17dd670c3153b05dbe1c398738a60bfb63589f9
pradyumn07 <123253911+pradyumn07@users.noreply.github.com>
2024-08-20 21:23:03 +0530

    Time: 10 ms (82.54%), Space: 10.3 MB (90.92%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[101][201];
+    int helper(int i,int M,vector<int>& piles){
+        if(i>=piles.size()) return 0;
+        if(dp[i][M]!=-1) return dp[i][M];
+        int total=0;
+        int ans=INT_MIN;
+        for(int j=0;j<2*M;j++){
+            if(i+j<piles.size()) total+=piles[i+j];
+            ans=max(ans,total-helper(i+j+1,max(M,j+1),piles));
+        }
+        return dp[i][M]=ans;
+    }
+    int stoneGameII(vector<int>& piles) {
+        int sum=0;
+        memset(dp,-1,sizeof dp);
+        for(auto a:piles) sum+=a;
+        int diff=helper(0,1,piles);
+        return (sum+diff)/2;
+    }
+};
