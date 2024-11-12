7dd2286922c66a0c6825d830a40fd953f13a844e
pradyumn07 <123253911+pradyumn07@users.noreply.github.com>
2024-11-12 19:27:56 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (26.06%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n = sentence.length();
+        if (sentence[0] != sentence[n - 1]) return false;
+        for(int i=0;i<sentence.length();i++){
+            if(sentence[i]==' '){
+                if(sentence[i-1]!=sentence[i+1]) return false;
+            }
+        }
+        
+        return true;
+    }
+};
