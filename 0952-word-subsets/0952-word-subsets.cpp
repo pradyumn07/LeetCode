ac6383ad8ac47dd840b928a54fe1828d91c82ccd
pradyumn07 <123253911+pradyumn07@users.noreply.github.com>
2025-01-10 12:34:57 +0530

    Time: 34 ms (72.15%), Space: 84.3 MB (72.15%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isSubset(vector<int>& freq2,vector<int>& temp){
+        for(int i=0;i<26;i++){
+            if(temp[i]<freq2[i]) return false;
+
+        }
+        return true;
+    }
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> ans;
+        vector<int> freq2(26);
+        for(string& word:words2){
+            int temp[26]={0};
+            for(char& ch:word){
+                temp[ch-'a']++;
+                freq2[ch-'a']=max(freq2[ch-'a'],temp[ch-'a']);
+            }
+        }
+        for(string& word:words1){
+            vector<int> temp(26,0);
+            for(char& ch:word){
+                temp[ch-'a']++;
+            }
+            if(isSubset(freq2,temp)==true){
+                ans.push_back(word);
+            }
+        }
+        return ans;
+
+    }
+};
