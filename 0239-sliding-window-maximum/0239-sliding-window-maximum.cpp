8f0112a61726fcdfd47987fb67ec26576ecc4984
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-03-31 11:59:36 +0530

    Time: 22 ms (67.2%), Space: 139 MB (93.93%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,29 @@
+
+class Solution {
+public:
+    int find(int i,int k,vector<int>& nums){
+        int m=INT_MIN;
+        for(int j=0;j<k;j++){
+            if(nums[i]>m) m=nums[i];
+            i++;
+        }
+        return m;
+    }
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> ans;
+        deque<int> dq;
+        for(int i=0;i<n;i++){
+            while(!dq.empty() && dq.front()<=i-k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[dq.back()]<nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i>=k-1) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+         
+    }
+};
