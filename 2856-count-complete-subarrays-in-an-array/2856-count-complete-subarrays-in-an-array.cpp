d99827dd1bddc0fc7f2da0cbdd2194bc8bf53e79
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-04-24 12:41:43 +0530

    Time: 18 ms (69.88%), Space: 41.8 MB (59.44%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -5,38 +5,20 @@ public:
         int n=nums.size();
         for(int i=0;i<nums.size();i++) st.insert(nums[i]);
         int count=st.size();
-        // unordered_set<int> stt;
-        // stt.insert(nums[0]);
-        // int c=0;
-        // int i=0,j=i+1;
-        // while(i<n){
-        //     if(count==stt.size()) c++;
-        //     // while(stt.size()>count){
-        //     //     stt.erase(stt.begin());
-        //     //     i++;
-        //     // }
-        //     if(j==n && i<n-1){
-        //         stt.clear();
-        //         stt.insert(nums[i+1]);
-
-        //         i++;
-        //         j=i+1;
-        //         continue;
-        //     }
-        //     stt.insert(nums[j]);
-        //     j++;
-
-
-        // }
-        // return c;
+        unordered_map<int,int> mp;
         int c=0;
-        for(int i=0;i<n;i++){
-            unordered_set<int> stt;
-            for(int j=i;j<n;j++){
-                stt.insert(nums[j]);
-                if(stt.size()==count) c++;
+        int i=0,j=0;
+        while(j<n){
+            mp[nums[j]]++;
+            while(mp.size()==count){
+                c+=n-j;
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0) mp.erase(nums[i]);
+                i++;
             }
+            j++;
         }
         return c;
+
     }
 };
