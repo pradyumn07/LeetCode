0e370d46cf80f0f7932f6320375cf64a9636e6b2
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-04-30 15:13:45 +0530

    Time: 19 ms (37.79%), Space: 11.2 MB (41.63%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,20 @@
 class Solution {
  public:
-  // Similar to 3. Longest Substring Without Repeating Characters
+ 
   int numberOfSubstrings(string s) {
     int ans = 0;
-    vector<int> count(3);
-
-    int l = 0;
-    for (const char c : s) {
-      ++count[c - 'a'];
-      while (count[0] > 0 && count[1] > 0 && count[2] > 0)
-        --count[s[l++] - 'a'];
-      // s[0..r], s[1..r], ..., s[l - 1..r] are satified strings.
-      ans += l;
+    int n=s.length();
+    unordered_map<char,int> mp;
+    int i=0,j=0;
+    while(j<n){
+        mp[s[j]]++;
+        while(mp['a']>=1 && mp['b']>=1 && mp['c']>=1){
+            ans+=n-j;
+            mp[s[i]]--;
+            i++;
+        }
+        j++;
     }
-
     return ans;
   }
 };
