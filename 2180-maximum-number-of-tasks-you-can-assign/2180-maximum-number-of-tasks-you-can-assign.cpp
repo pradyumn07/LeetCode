9bab2e2d347503d9de72b461963fed7d16d59d1a
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-05-01 15:11:20 +0530

    Time: 829 ms (33.72%), Space: 286.3 MB (65.12%) - LeetHub

---
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool check(vector<int>& tasks,vector<int>& workers,int pills,int strength,int mid){
+        int usedpills=0;
+        multiset<int> st(begin(workers),begin(workers)+mid);
+        for(int i=mid-1;i>=0;i--){
+            int req=tasks[i];
+            auto it=prev(st.end());
+            if(*it>=req) st.erase(it);
+            else if(usedpills>=pills) return false;
+            else {
+                auto weakest=st.lower_bound(req-strength);
+                if(weakest==st.end()) return false;
+                st.erase(weakest);
+                usedpills++;
+            }
+        }
+        return true;
+    }
+    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        int m = tasks.size();
+        int n = workers.size();
+
+        int l = 0;
+        int r = min(m, n);
+
+        sort(begin(tasks), end(tasks));
+        sort(begin(workers), end(workers), greater<int>());
+
+        int result = 0;
+
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+
+            if(check(tasks, workers, pills, strength, mid)) {
+                result = mid;
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+
+        return result;
+
+
+    }
+};
