fb48a9a20d8cbb9505244a0d7b8f7457d6a16698
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-06-11 18:12:36 +0530

    Time: 5 ms (83.2%), Space: 22.8 MB (74.02%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -8,25 +8,22 @@ public:
         }
         return maxi;
     }
-    long long calculate(vector<int> &piles,int hours){
+    long long calculate(vector<int> &piles,int m){
         long long total=0;
-        int n=piles.size();
-        for(int i=0;i<n;i++){
-            total+=ceil((double)piles[i]/(double)hours);
+        for(int i=0;i<piles.size();i++){
+            total+=piles[i]/m;
+            if(piles[i]%m!=0) total+=1;
         }
         return total;
     }
 
     int minEatingSpeed(vector<int>& piles, int h) {
         int low=1,high=findMax(piles);
-        while(low<=high){
+        while(low<high){
             int mid=(low+high)/2;
             long long total=calculate(piles,mid);
-            if(total<=h){
-                high=mid-1;
-            } else{
-                low=mid+1;
-            }
+            if(total<=h) high=mid;
+            else low=mid+1;
         }
         return low;
     }
