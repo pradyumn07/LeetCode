c50d818407d500f4550f4d0a21b5751b66370b83
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-07-15 15:45:13 +0530

    Time: 22 ms (61.48%), Space: 23.6 MB (32.89%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,13 +11,9 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL) return NULL;
-        int current=root->val;
-        if(current<p->val && current<q->val){
-            return lowestCommonAncestor(root->right,p,q);
-        }
-        if(current>p->val && current>q->val){
-            return lowestCommonAncestor(root->left,p,q);
+        while((root->val<p->val && root->val<q->val) || (root->val>p->val && root->val>q->val)){
+            if(root->val<p->val && root->val<q->val) root=root->right;
+            else if(root->val>p->val && root->val>q->val) root=root->left;
         }
         return root;
     }
