75126d8c2624f36c91d0adc7ba4a742cb03daaa6
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-07-17 13:49:36 +0530

    Time: 0 ms (100%), Space: 10.1 MB (90.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    
-    int findWays(int n,vector<int> &nums,vector<int> &dp){
-        if(n==0) return  nums[0];
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        
-        int p=findWays(n-2,nums,dp)+nums[n];
-        int np=findWays(n-1,nums,dp)+0;
-        return dp[n]=max(p,np);
+    int dp[401];
+    int solve(int ind,vector<int>& nums){
+        if(ind==0) return nums[ind];
+        if(ind<0) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int pick=nums[ind]+solve(ind-2,nums);
+        int notpick=0+solve(ind-1,nums);
+        return dp[ind]=max(pick,notpick);
     }
     int rob(vector<int>& nums) {
-        
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return findWays(n-1,nums,dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(n-1,nums);
     }
 };
