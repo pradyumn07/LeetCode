b7c31964e0fcab87fb382500afd744e2db476126
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-07-20 13:55:44 +0530

    Time: 30 ms (59.24%), Space: 44.4 MB (22.29%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -5,16 +5,13 @@ public:
         int m=t.length();
         vector<vector<double>> dp(n+1,vector<double>(m+1,0));
         for(int i=0;i<=n;i++) dp[i][0]=1;
-        for(int j=1;j<=m;j++) dp[0][j]=0;     //start with j=1 because we have already taken a base case above that in which j=0 we have set to 1
+        for(int j=1;j<=m;j++) dp[0][j]=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                } else {
-                    dp[i][j]=dp[i-1][j];
-                }
+                if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else dp[i][j]=dp[i-1][j];
             }
         }
-        return (int)dp[n][m];  //kyunki isko bhejkar pehle call kia tha
+        return (int)dp[n][m];
     }
 };
