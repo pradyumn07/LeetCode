2ec7aee54419a550e0e0376c821301ae8e154971
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-07-27 12:19:44 +0530

    Time: 11 ms (6.78%), Space: 12.3 MB (91.85%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
+    vector<int> nge(vector<int> &nums2){
+        vector<int> arr(nums2.size());
+        int n=nums2.size();
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums2[i]) st.pop();
+            if(st.empty()) arr[i]=-1;
+            else{
+                arr[i]=st.top();
+
+            }
+            st.push(nums2[i]);
+        }
+        return arr;
+    }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
+        vector<int> a=nge(nums2);
+        vector<int> temp(nums1.size());
         for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
                 if(nums1[i]==nums2[j]){
-                    int k=j+1;
-                    int f=0;
-                    while(k<nums2.size()){
-                        if(nums1[i]<nums2[k]){
-                            f=1;
-                            ans.push_back(nums2[k]);
-                            break;
-                        }
-                        k++;
-                    }
-                    if(f==0) ans.push_back(-1);
+                    temp[i]=a[j];
                 }
             }
         }
-        return ans;
+        return temp;
     }
 };
