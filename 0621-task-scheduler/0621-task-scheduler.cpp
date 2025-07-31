18998e87d2f275143439116ea25bd5118961b336
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-07-31 11:47:50 +0530

    Time: 38 ms (34.65%), Space: 47.8 MB (9.97%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> mp(26,0);
+        for(char& ch:tasks){
+            mp[ch-'A']++;
+
+        }
+        int time=0;
+        priority_queue<int> pq;
+        for(int i=0;i<26;i++){
+            if(mp[i]>0) pq.push(mp[i]);    
+        }
+        while(!pq.empty()){
+            vector<int> temp;
+            for(int i=1;i<=n+1;i++){
+                if(!pq.empty()){
+                    int fre=pq.top();
+                    pq.pop();
+                    fre--;
+                    temp.push_back(fre);
+                }
+            }
+            for(int &f:temp){
+                if(f>0) pq.push(f);
+            }
+            if(pq.empty()) time+=temp.size();
+            else time+=n+1;
+        }
+        return time;
+    }
+
+};
