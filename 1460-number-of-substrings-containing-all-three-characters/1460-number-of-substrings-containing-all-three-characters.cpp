27825793495e430b1ba21786d22609b3373f25e4
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-07-31 19:09:17 +0530

    Time: 33 ms (18.13%), Space: 16.8 MB (8.35%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,20 +1,20 @@
 class Solution {
- public:
- 
-  int numberOfSubstrings(string s) {
-    int ans = 0;
-    int n=s.length();
-    unordered_map<char,int> mp;
-    int i=0,j=0;
-    while(j<n){
-        mp[s[j]]++;
-        while(mp['a']>=1 && mp['b']>=1 && mp['c']>=1){
-            ans+=n-j;
-            mp[s[i]]--;
-            i++;
+public:
+    int numberOfSubstrings(string s) {
+        int l=0,r=0;
+        int cnt=0;
+        unordered_map<char,int> mp;
+        int n=s.length();
+        while(r<s.length()){
+             mp[s[r]]++;
+             while(mp.size()==3){
+                cnt+=n-r;
+                mp[s[l]]--;
+                if(mp[s[l]]==0) mp.erase(s[l]);
+                l++;
+             } 
+             r++;
         }
-        j++;
+        return cnt;
     }
-    return ans;
-  }
 };
