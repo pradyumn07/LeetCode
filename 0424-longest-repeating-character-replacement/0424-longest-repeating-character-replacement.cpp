48300cb647eaca0502f2416a689b73d734c99ff5
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-07-31 17:56:33 +0530

    Time: 3 ms (70.95%), Space: 10.9 MB (40.28%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l=0,r=0,maxf=0,maxl=0;
-        vector<int> mp(26,0);
-        while(r<s.length()){
-            mp[s[r]-'A']++;
-            maxf=max(maxf,mp[s[r]-'A']);
-            if(((r-l+1)-maxf)>k){
-                mp[s[l]-'A']--;
-                l=l+1;
+        int l=0,r=0;
+        int n=s.length();
+        int maxF=0;
+        vector<int> f(26,0);
+        int ans=0;
+        while(r<n){
+            f[s[r]-'A']++;
+            maxF=max(maxF, f[s[r]-'A']);
+            if((r-l+1)-maxF>k){
+                f[s[l]-'A']--;
+                l++;
             }
-            if(((r-l+1)-maxf)<=k){
-                maxl=max(maxl,r-l+1);
-                r++;
-            }
-           
+            ans=max(ans,r-l+1);
+            r++;
+
         }
-         return maxl;
+        return ans;
     }
 };
