251396d14b2f5d488703180e21b0be2f37342fd9
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-08-07 12:54:36 +0530

    Time: 4 ms (71.27%), Space: 12.5 MB (61.84%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int dp[501];
-    int solve(int i,int n,vector<int>& arr,int k){
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int maxi=INT_MIN,sum=0,len=0,maxSum=0;
-        for(int j=i;j<min(n,i+k);j++){
-            len++;
-            maxi=max(maxi,arr[j]);
-            sum=(len*maxi)+solve(j+1,n,arr,k);
-            maxSum=max(maxSum,sum);
-        }
-        return dp[i]=maxSum;
-    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,n,arr,k);
+        vector<int> dp(n+1,-1);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int len=0,maxi=INT_MIN,maxans=INT_MIN;
+            for(int j=i;j<min(n,i+k);j++){
+                len++;
+                maxi=max(maxi,arr[j]);
+                int sum=len*maxi+dp[j+1];
+                maxans=max(maxans,sum);
+            }
+            dp[i]=maxans;
+        }
+        return dp[0];
     }
 };
