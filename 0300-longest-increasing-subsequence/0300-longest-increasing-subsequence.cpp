43a763ec5e50a11e85932e4b1e0b36bd961796f0
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-08-08 12:52:48 +0530

    Time: 0 ms (100%), Space: 14.1 MB (80.22%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    int n;
-    int dp[2502][2502];
-    int solve(int p,int ind,vector<int>& nums){
-        if(ind>=n) return 0;
-        if(p!=-1 && dp[ind][p]!=-1) return dp[ind][p];
-        int take=0;
-        if(p==-1 || nums[p]<nums[ind]){
-            take=1+solve(ind,ind+1,nums);
-        }
-        int skip=solve(p,ind+1,nums);
-        if(p!=-1) dp[ind][p]=max(take,skip);
-        return max(take,skip);
-    }
+    // int n;
+    // int dp[2502][2502];
+    // int solve(int p,int ind,vector<int>& nums){
+    //     if(ind>=n) return 0;
+    //     if(p!=-1 && dp[ind][p]!=-1) return dp[ind][p];
+    //     int take=0;
+    //     if(p==-1 || nums[p]<nums[ind]){
+    //         take=1+solve(ind,ind+1,nums);
+    //     }
+    //     int skip=solve(p,ind+1,nums);
+    //     if(p!=-1) dp[ind][p]=max(take,skip);
+    //     return max(take,skip);
+    // }
     int lengthOfLIS(vector<int>& nums) {
-        n=nums.size();
+        // n=nums.size();
 
-        memset(dp,-1,sizeof(dp));
-        return solve(-1,0,nums);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(-1,0,nums);
+        int n=nums.size();
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int len=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>temp.back()){
+                len++;
+                temp.push_back(nums[i]);
+            } else {
+                int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[ind]=nums[i];
+            }
+        }
+        return len;
     }
 };
