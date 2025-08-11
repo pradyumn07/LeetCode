864f2e754b03f8e5177e3337ecfd0fc71a7948d7
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-08-12 00:06:39 +0530

    Time: 11 ms (14.28%), Space: 25 MB (8.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n=intervals.size();
-        sort(begin(intervals),end(intervals));
-        vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-        for(int i=1;i<n;i++){
-            if(intervals[i][0]<=ans.back()[1]){
-                ans.back()[0]=min(ans.back()[0],intervals[i][0]);
-                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+        vector<vector<int>> mergeinter;
+        sort(intervals.begin(),intervals.end());
+        vector<int> temp=intervals[0];
+        for(auto it:intervals){
+            if(it[0]<=temp[1]){
+                temp[1]=max(it[1],temp[1]);
             } else {
-                ans.push_back(intervals[i]);
+                mergeinter.push_back(temp);
+                temp=it;
             }
+            
         }
-        return ans;
+        mergeinter.push_back(temp);
+        return mergeinter;
     }
 };
