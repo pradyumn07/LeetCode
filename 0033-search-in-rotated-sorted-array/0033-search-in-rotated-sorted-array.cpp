857a02ff2e6ad920a0122fc8b2333b846ec7fa2c
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-08-14 12:12:32 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            
-            if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[low]<=nums[mid]){
-                    if(nums[low]<=target && nums[mid]>=target){high=mid-1;}
-                    else {low=mid+1;}
-                }
-            else {
-                if(target>=nums[mid] && target<=nums[high]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-            }
+    int bs(int l,int r,vector<int> &nums,int target){
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) r=mid-1;
+            else l=mid+1;
         }
         return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0,r=nums.size()-1;
+        int ans=0;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[r]){
+                l=mid+1;
+            } else {
+                ans=r;
+                r=mid;
+            }
+        }
+        int pivot = l; 
+        if(target >= nums[pivot] && target <= nums[n - 1]){
+            return bs(pivot, n - 1, nums, target);
+        } else {
+            return bs(0, pivot - 1, nums, target);
+        }
+        
     }
 };
