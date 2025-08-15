7943141e7bbd19719e2610d90b574c28095cda7e
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-08-15 15:13:48 +0530

    Time: 0 ms (100%), Space: 8 MB (18.14%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0) return false;
+        int x=log(n)/log(4);
+        if(n==pow(4,x)) return true;
+        return false;
+    }
+};
