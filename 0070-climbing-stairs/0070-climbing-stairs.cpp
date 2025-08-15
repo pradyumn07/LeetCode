ce64226a8be13141f4507d76ae6d96a2496c50d4
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-08-16 00:43:53 +0530

    Time: 0 ms (100%), Space: 7.9 MB (73.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,10 +3,11 @@ public:
     int dp[46];
     int solve(int ind){
         if(ind==0) return 1;
-        if(ind ==1) return 1;
+        if(ind==1) return 1;
         if(dp[ind]!=-1) return dp[ind];
         int one=solve(ind-1);
-        int two=solve(ind-2);
+        int two;
+        if(ind>=2) two=solve(ind-2);
         return dp[ind]=one+two;
     }
     int climbStairs(int n) {
