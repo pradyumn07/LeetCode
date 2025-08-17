8527781aa21befeba37dfc5d49b0095ac5282e90
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-08-17 18:49:09 +0530

    Time: 27 ms (36.85%), Space: 26.7 MB (89.94%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int n=str1.length();
-        int m=str2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++) dp[i][0]=0;
-        for(int j=0;j<=m;j++) dp[0][j]=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+        int m=str1.length();
+        int n=str2.length();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<n;i++) dp[0][i]=0;
+        for(int i=0;i<m;i++) dp[i][0]=0;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
                 if(str1[i-1]==str2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
 
+                else {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
+        int i=m,j=n;
         string ans="";
-        int i=n,j=m;
         while(i>0 && j>0){
             if(str1[i-1]==str2[j-1]){
                 ans+=str1[i-1];
                 i--;
                 j--;
-            } else if(dp[i-1][j]<dp[i][j-1]){
-                ans+=str2[j-1];
-                j--;
-            } else {
+            } else if(dp[i-1][j]>dp[i][j-1]){
                 ans+=str1[i-1];
                 i--;
+            } else {
+                ans+=str2[j-1];
+                j--;
             }
         }
         while(i>0){
-            ans+=str1[i-1]; i--;
+            ans+=str1[i-1];
+            i--;
         }
         while(j>0){
             ans+=str2[j-1];
@@ -37,6 +40,5 @@ public:
         }
         reverse(ans.begin(),ans.end());
         return ans;
-
     }
 };
