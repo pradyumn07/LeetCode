ee5abe9af148421f7df12f923f816b7f5d5d3e56
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-08-17 17:59:34 +0530

    Time: 47 ms (90.26%), Space: 13 MB (84.51%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.length();
-        int m=text2.length();
-        vector<int> prev(m+1,0),cur(m+1,0);
-
-        for(int j=0;j<=m;j++) prev[j]=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(text1[i-1]==text2[j-1]) cur[j]=1+prev[j-1];
-                else cur[j]=max(prev[j],cur[j-1]);
-            }
-            prev=cur;
-        }
-        return prev[m];
+    int n;
+    int dp[1001][1001];
+    int solve(int i,int j,string &s,string &t){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==t[j]) return dp[i][j]=1+solve(i-1,j-1,s,t);
+        return dp[i][j]=max(solve(i-1,j,s,t),solve(i,j-1,s,t));
     }
     int longestPalindromeSubseq(string s) {
-        int n=s.length();
         string t=s;
-        reverse(t.begin(),t.end());
-        return longestCommonSubsequence(s,t);
+        reverse(s.begin(),s.end());
+        n=s.length();
+        memset(dp,-1,sizeof(dp));
+        return solve(n-1,n-1,s,t);
     }
 };
