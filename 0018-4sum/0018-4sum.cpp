ac4cbcc36ee78d0006b98a1b67a76771679eb70d
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-08-22 00:31:53 +0530

    Time: 23 ms (56.4%), Space: 17.4 MB (56.81%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -5,9 +5,9 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            if(i!=0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                if(j!=i+1 && nums[j]==nums[j-1]) continue;
                 int k=j+1;
                 int l=n-1;
                 while(k<l){
@@ -27,6 +27,7 @@ public:
                     else l--;
                 }
             }
+
         }
         return ans;
     }
