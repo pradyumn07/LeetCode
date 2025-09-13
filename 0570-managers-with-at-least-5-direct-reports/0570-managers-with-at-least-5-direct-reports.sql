11d5b01da538ffce107b1bd9ce470ef50bdcda8c
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-09-13 18:53:02 +0530

    Time: 336 ms (69%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-select e1.name from Employee as e1 join Employee as e2
-on e1.id=e2.managerId 
+select e1.name from Employee as e1 join Employee as e2 on e1.id=e2.managerId
 group by e2.managerId
-having count(e2.managerId)>4; 
+having count(e2.managerId)>4 
