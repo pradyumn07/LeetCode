a872bd618ffccfb92bb0c07fdd42543e549803dc
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-09-27 21:32:59 +0530

    Time: 39 ms (33.89%), Space: 18.4 MB (81.02%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,30 +1,43 @@
 class Solution {
- public:
-  int removeStones(vector<vector<int>>& stones) {
-    int numOfIslands = 0;
-    vector<vector<int>> graph(stones.size());
-    unordered_set<int> seen;
+public:
+    vector<int> parent;
+    vector<int> rank;
+    int find(int x){
+        if(x==parent[x]) return x;
+        return parent[x]=find(parent[x]);
 
-    for (int i = 0; i < stones.size(); ++i)
-      for (int j = i + 1; j < stones.size(); ++j)
-        if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
-          graph[i].push_back(j);
-          graph[j].push_back(i);
+    }
+    void Union(int x,int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
+        if(x_parent==y_parent) return;
+        if(rank[x_parent]>rank[y_parent]) parent[y_parent]=x_parent;
+        else if(rank[x_parent]<rank[y_parent]) parent[x_parent]=y_parent;
+        else{
+            parent[x_parent]=y_parent;
+            rank[y_parent]++;
         }
+    }
 
-    for (int i = 0; i < stones.size(); ++i)
-      if (seen.insert(i).second) {
-        dfs(graph, i, seen);
-        ++numOfIslands;
-      }
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+        parent.resize(n);
+        rank.resize(n,0);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
+                    Union(i,j);
+                }
+            }
+        }
+        int groups=0;
+        for(int i=0;i<n;i++){
+            if(i==parent[i]) groups++;
+        }
+        return n-groups;
 
-    return stones.size() - numOfIslands;
-  }
-
- private:
-  void dfs(const vector<vector<int>>& graph, int u, unordered_set<int>& seen) {
-    for (const int v : graph[u])
-      if (seen.insert(v).second)
-        dfs(graph, v,seen);
-  }
+    }
 };
