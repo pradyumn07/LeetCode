446abb3ec269786f7729c06d6ed8871fd64447ad
Pradyumn Niranjan <123253911+pradyumn07@users.noreply.github.com>
2025-10-01 20:03:03 +0530

    Time: 0 ms (100%), Space: 22.1 MB (60.92%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,34 +2,20 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> st;
+        vector<int> ans;
         for(int i=0;i<asteroids.size();i++){
-            bool destroyed = false;
-
-            while (!st.empty() && st.top() > 0 && asteroids[i] < 0) {
-                int top = st.top();
-                if (abs(top) < abs(asteroids[i])) {
-                    st.pop(); // top gets destroyed
-                    continue; // current asteroid may still collide with more
-                } else if (abs(top) == abs(asteroids[i])) {
-                    st.pop(); // both destroy each other
-                    destroyed = true;
-                    break;
-                } else {
-                    destroyed = true; // current gets destroyed
-                    break;
-                }
-            }
-
-            if (!destroyed) {
-                st.push(asteroids[i]);
+            if(asteroids[i]>0) st.push(asteroids[i]);
+            else{
+                while(!st.empty() && st.top()>0 && st.top()<abs(asteroids[i])) st.pop();
+                if(!st.empty() && st.top()==abs(asteroids[i])) st.pop();
+                else if(st.empty() || st.top()<0) st.push(asteroids[i]);
             }
         }
-        vector<int> temp;
         while(!st.empty()){
-            temp.push_back(st.top());
+            ans.push_back(st.top());
             st.pop();
         }
-        reverse(temp.begin(),temp.end());
-        return temp;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
